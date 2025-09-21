bcdf66d1d590650490aee4ec5a816147d5725337
GreatDeveloper66 <ashaffe@gmail.com>
2025-09-20 23:04:22 -0500

    Change App.js to new react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import LoginForm from './UserAuthForms/LoginForm';
+import Dashboard from './Pages/Dashboard';
 import './App.css';
-
+/**
+ * Main application component that defines the routing structure for the frontend.
+ */
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </div>
-  );
-}
+  )
+};
 
 export default App;
+
