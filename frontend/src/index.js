9ab91706a798123f08f89f43c6e719785ba69b32
GreatDeveloper66 <ashaffe@gmail.com>
2025-09-25 11:25:38 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { AuthProvider } from "./Context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <AuthProvider>
     <App />
-  </BrowserRouter>
+  </AuthProvider>
 );
