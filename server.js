beb147d24bd7fee9344c64c4ab77a3d8613e6b97
GreatDeveloper66 <ashaffe@gmail.com>
2025-09-25 11:25:10 -0500

    Update string specifying route for main get route

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ app.get("/api/hello", (req, res) => {
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
 // Catch-all route: send React index.html for any unknown route
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
 });
+
+
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
