24d5a4c45d23b728c159040276cca3576321063d
GreatDeveloper66 <ashaffe@gmail.com>
2025-09-25 11:25:29 -0500

    Update App.js with new router tree

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import LoginForm from './UserAuthForms/LoginForm';
 import Dashboard from './Pages/Dashboard';
 import './App.css';
+
 /**
  * Main application component that defines the routing structure for the frontend.
  */
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
-  )
-};
+  );
+}
 
 export default App;
 
